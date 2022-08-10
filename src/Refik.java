6aae36db90448ba016a1be15cfedd15d1dea64c8
refikturk <rfkturk43@gmail.com>
2022-08-10 20:47:06 +0300

    First commit by Refik

---
+++ src/Refik.java
@@ -0,0 +1,9 @@
+public class Refik {
+
+    public static void main(String[] args) {
+
+        System.out.println("First change from Refik");
+        System.out.println("Second change from Refik");
+
+    }
+}
